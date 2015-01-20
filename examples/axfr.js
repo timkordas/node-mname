5fad6e385b65cd0e7b94653419116ca5ec24133f
Alex Wilson <alex@cooperi.net>
2015-01-20 15:51:14 +1000

    Add quick axfr example code

---
+++ examples/axfr.js
@@ -0,0 +1,28 @@
+var named = require('./lib/index');
+var bunyan = require('bunyan');
+var log = bunyan.createLogger({name: 'test'});
+
+var server = named.createServer();
+var ttl = 3600;
+
+server.listen(9953, '127.0.0.1');
+server.listenTcp(9953, '127.0.0.1');
+
+server.on('query', function(query) {
+	var domain = query.name();
+	log.info({name: query.name(), type: query.type()});
+
+	var soa = new named.SOARecord(domain, {serial: 12345});
+	query.addAnswer(domain, soa, ttl);
+	server.send(query);
+
+	var a = new named.ARecord("1.2.3.4");
+	var a2 = new named.ARecord("1.2.3.5");
+	query.addAnswer("foo." + domain, a, ttl);
+	query.addAnswer("bar." + domain, a, ttl);
+	query.addAnswer("foobar." + domain, a2, ttl);
+	server.send(query);
+
+	query.addAnswer(domain, soa, ttl);
+	server.send(query);
+});
