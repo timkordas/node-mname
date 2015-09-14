28f95211837d71d708aabbabfe2223eb2377edf2
Alex Wilson <alex.wilson@joyent.com>
2015-09-14 16:14:21 -0700

    Example for how to use IXFR support

--- examples/axfr.js
+++ examples/axfr.js
@@ -1,4 +1,4 @@
-var named = require('./lib/index');
+var named = require('../lib/index');
 var bunyan = require('bunyan');
 var log = bunyan.createLogger({name: 'test'});
 
@@ -12,17 +12,44 @@ server.on('query', function(query) {
 	var domain = query.name();
 	log.info({name: query.name(), type: query.type()});
 
-	var soa = new named.SOARecord(domain, {serial: 12345});
-	query.addAnswer(domain, soa, ttl);
-	server.send(query);
+	if (query.type() === 'AXFR') {
+		var soa = new named.SOARecord(domain, {serial: 12345});
+		query.addAnswer(domain, soa, ttl);
+		server.send(query);
 
-	var a = new named.ARecord("1.2.3.4");
-	var a2 = new named.ARecord("1.2.3.5");
-	query.addAnswer("foo." + domain, a, ttl);
-	query.addAnswer("bar." + domain, a, ttl);
-	query.addAnswer("foobar." + domain, a2, ttl);
-	server.send(query);
+		var a = new named.ARecord("1.2.3.4");
+		var a2 = new named.ARecord("1.2.3.5");
+		query.addAnswer("foo." + domain, a, ttl);
+		query.addAnswer("bar." + domain, a, ttl);
+		query.addAnswer("foobar." + domain, a2, ttl);
+		server.send(query);
 
-	query.addAnswer(domain, soa, ttl);
-	server.send(query);
+		query.addAnswer(domain, soa, ttl);
+		server.send(query);
+
+	} else if (query.type() === 'IXFR') {
+		var base = query.ixfrBase();
+		var oldSoa = new named.SOARecord(domain, {serial: base});
+		var newSoa = new named.SOARecord(domain, {serial: 12345});
+		query.addAnswer(domain, newSoa, ttl);
+		server.send(query);
+
+		var a = new named.ARecord("1.2.3.4");
+		var a2 = new named.ARecord("1.2.3.5");
+
+		/* removed since old serial */
+		query.addAnswer(domain, oldSoa, ttl);
+		server.send(query);
+		query.addAnswer("tri." + domain, a, ttl);
+
+		/* new additions */
+		query.addAnswer(domain, newSoa, ttl);
+		query.addAnswer("foo." + domain, a, ttl);
+		query.addAnswer("bar." + domain, a, ttl);
+		query.addAnswer("foobar." + domain, a2, ttl);
+		server.send(query);
+
+		query.addAnswer(domain, newSoa, ttl);
+		server.send(query);
+	}
 });
