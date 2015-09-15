622d93bd2ddf50187b5d5011700fb25a1a851a25
Alex Wilson <alex.wilson@joyent.com>
2015-09-15 15:41:41 -0700

    Basic EDNS support, clean up and refactor Query class

--- examples/reflect.js
+++ examples/reflect.js
@@ -47,5 +47,5 @@ server.on('clientError', function(error) {
 });
 
 server.on('uncaughtException', function(error) {
-        console.log("there was an excepton: %s", error.message());
+        console.log("there was an excepton: %s", error.message);
 });
